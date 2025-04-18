589cddd04113fe51e65c5ad30ee658e53f222875
Hitesh-mca <hitesh_mca1988@yahoo.com>
2025-04-18 01:00:37 +0000

    Variable & Datatypr

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,11 @@
+const accountID=123455
+let accountEmail='abc@gmail.com'
+var accountPassword='12345'
+accountCity='Australia'
+let accountState
+//accountID=23 // not allowed
+console.log(accountID);
+accountEmail='xyz@gmail.com'
+accountPassword='567'
+accountCity='India'
+console.table([accountID, accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,5 @@
+"use strict"; // treat all js code as newer version
+//alert('hello') not run bcz we use node js not browser
+let name='hitesh'
+let age=123
+let isLoggedIn=false

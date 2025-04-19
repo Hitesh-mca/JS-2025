67ce0a2b4730a5c5d28aafbeabfc8f4ff7593c27
Hitesh-mca <hitesh_mca1988@yahoo.com>
2025-04-19 04:55:12 +0000

    String

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,7 @@
+const name=new String ('Hitesh Patel') 
+//const name='Hitesh';
+const age =35
+
+console.log('My name is ' + name + "and age is " + age) // way 1
+console.log(`my name is ${name.toUpperCase()} and age is ${age}`);// way 2 it is good way to use it
+console.log(name.length);

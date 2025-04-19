1071e3861203d92e06209e3d5a9ea66f45557ee3
Hitesh-mca <hitesh_mca1988@yahoo.com>
2025-04-19 00:11:58 +0000

    Conversion OF Data Type

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score=45
+let age="33"
+
+console.log(typeof score);
+console.log(typeof age);
+
+let valueInNumber=Number(age)
+console.log(typeof valueInNumber)
+
+/* ''33" =>33
+    ''33abc" = > NaN
+    true =>1
+    false =>0
+*/
+
+let isLoggedIn=" "
+let booleIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleIsLoggedIn);
+
+// 1=>true
+// 0=>false
+//"hitesh"=> true
+//""=>false
+
+let salary=4500
+console.log(typeof salary);
+
+let stringNumber=String(salary)
+console.log(stringNumber, typeof stringNumber);

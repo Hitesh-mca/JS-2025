6799f7d332204b47b3bd69d335c7723ef2f53273
Hitesh-mca <hitesh_mca1988@yahoo.com>
2025-05-14 04:39:21 +0000

    if concept

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,15 @@
+if (2==2){
+    console.log("Executed-1");
+    
+}
+//check datatype as well
+if(2==="2"){ 
+console.log("Executed-2");
+}
+
+//check 2 is not equel to 3 meas give true value and run if
+if(2 !=3){ 
+console.log("Executed-3");
+}
+
+

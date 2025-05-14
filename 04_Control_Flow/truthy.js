6ba85e99a3a23d59a4c9bfbdcc1282bf6dda77f9
Hitesh-mca <hitesh_mca1988@yahoo.com>
2025-05-14 04:45:27 +0000

    Truthy concept

---
+++ 04_Control_Flow/truthy.js
@@ -0,0 +1,20 @@
+const userEmail="Hitesh123@gmail.com"
+
+//it assumeed true 
+if(userEmail){
+    console.log("Got user email");
+    
+}else{
+    console.log("Don't have user email");
+   
+}
+
+//if value like empty it assumed false
+const userEmail1="";
+if(userEmail1){
+    console.log("Got user email");
+    
+}else{
+    console.log("Don't have user email");
+    
+}

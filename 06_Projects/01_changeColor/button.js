8755f098304ddd6a88d01866977781252cc17b8c
Hitesh-mca <hitesh_mca1988@yahoo.com>
2025-07-02 00:15:13 +0000

    Project-01 Change BG Color using button.

---
+++ 06_Projects/01_changeColor/button.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('.bt')
+const body=document.querySelector('body')
+
+ buttons.forEach(function(bt){
+   // console.log(bt);
+    bt.addEventListener('click',function(e){
+        console.log(e.target);
+        if(e.target.id==='grey'){
+            body.style.backgroundColor=e.target.id
+        }
+        if(e.target.id==='red'){
+            body.style.backgroundColor=e.target.id
+        }
+        if(e.target.id==='green'){
+            body.style.backgroundColor=e.target.id
+        }
+        if(e.target.id==='blue'){
+            body.style.backgroundColor=e.target.id
+        }
+    })
+ })
---
+++ 06_Projects/projectSetOne.md
@@ -0,0 +1,3 @@
+# Project related to DOM
+## Project link
+[Click here](https://starckblitz.com)
